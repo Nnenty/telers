11a9fd177c77b2845e93111bc63cb0a5b7fe207d
Desiders <abc_desi@proton.me>
2024-04-04 21:22:00 +0300

    Add mixed-format sticker packs

--- telers/src/types/sticker_set.rs
+++ telers/src/types/sticker_set.rs
@@ -13,12 +13,8 @@ pub struct StickerSet {
     pub title: Box<str>,
     /// Type of stickers in the set, currently one of 'regular', 'mask', 'custom_emoji'
     pub sticker_type: Box<str>,
-    /// `true`, if the sticker set contains [`animated stickers`](https://telegram.org/blog/animated-stickers)
-    pub is_animated: bool,
-    /// `true`, if the sticker set contains [`video stickers`](https://telegram.org/blog/video-stickers-better-reactions)
-    pub is_video: bool,
     /// List of all set stickers
-    pub stickers: Vec<Sticker>,
+    pub stickers: Box<[Sticker]>,
     /// Sticker set thumbnail in the .WEBP, .TGS, or .WEBM format
     pub thumbnail: Option<PhotoSize>,
 }
