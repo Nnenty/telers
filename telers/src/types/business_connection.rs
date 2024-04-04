45d97686ecc08441e369590f94294a931c885a94
Desiders <abc_desi@proton.me>
2024-04-04 19:37:52 +0300

    Fix incorrect field type and name

--- telers/src/types/business_connection.rs
+++ telers/src/types/business_connection.rs
@@ -11,7 +11,7 @@ use serde::Deserialize;
 #[event(try_from = Update)]
 pub struct BusinessConnection {
     /// Unique identifier of the business connection
-    pub id: i64,
+    pub id: String,
     /// Business account user that created the business connection
     pub user: User,
     /// Identifier of a private chat with the user who created the business connection. This number may have more than 32 significant bits and some programming languages may have difficulty/silent defects in interpreting it. But it has at most 52 significant bits, so a 64-bit integer or double-precision float type are safe for storing this identifier.
@@ -21,7 +21,7 @@ pub struct BusinessConnection {
     /// `true`, if the bot can act on behalf of the business account in chats that were active in the last 24 hours
     pub can_reply: bool,
     /// `true`, if the connection is active
-    pub is_active: bool,
+    pub is_enabled: bool,
 }
 
 impl TryFrom<Update> for BusinessConnection {
