5635f79e898acde6222d3eee2e3a7f364bf5caa7
Desiders <abc_desi@proton.me>
2024-08-15 00:58:17 +0300

    Fix mention usage

--- telers/src/filters/command.rs
+++ telers/src/filters/command.rs
@@ -303,7 +303,7 @@ impl Command<'_> {
         } else if let Some(ref mention) = command.mention {
             bot.send(GetMe {}).await.map(|user| {
                 // `unwrap` is safe here, because bot always has username
-                user.username.unwrap().eq(mention.as_ref())
+                user.username.unwrap().eq(mention)
             })
         } else {
             Ok(true)
